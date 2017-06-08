cea96acb89900b4889e098368a986da51db8618e
Anne <anne.cognet@holbertonschool.com>
2017-06-08 22:24:06 +0000

    fix integer division

--- collatz/collatz.py
+++ collatz/collatz.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
+import sys
+
 
 def collatz(num):
     if not num:
         return ("nothing passed to function")
-    if not isinstance(num, int):
-        return ("input must be an integer")
     count = 0
     while num != 1:
         if num % 2 == 0:
-            num /= 2
+            num = num // 2
             count += 1
         else:
             num = (3*num) + 1
             count += 1
     return count
-print(collatz(75128138247))
-print(collatz(2361235441021745907775))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 collatz.py number")
+    else:
+        try:
+            count = int(sys.argv[1])
+        except TypeError:
+            print("input must be an integer")
+        print(collatz(count))
